42f7560200f442cc3bc953911e766109cd19ca1b
Luis Quiroz <luisqpra@gmail.com>
2023-09-23 10:49:23 -0500

    feat(Creacion): validacion si ya se han creado los datos en match y teams

--- _drop_bd.py
+++ _drop_bd.py
@@ -1,4 +1,3 @@
-from sqlalchemy.orm import sessionmaker
 from config import database_url
 from models import Match
 # from models import Team
@@ -6,13 +5,8 @@ from sqlalchemy import create_engine
 
 engine = create_engine(database_url)
 
-Session = sessionmaker(bind=engine)
-session = Session()
-
-
 if __name__ == '__main__':
-    # teams = session.query(Team).delete()
-    matchs = session.query(Match).delete()
-    session.commit()
+    # Team.__table__.drop(engine)
+    Match.__table__.drop(engine)
     print("Base de datos elimindada")
-    session.close()
+    engine.dispose()
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from config import database_url
-from utils import insert_teams_with_random_stats, get_team_ids, create_season
-from sqlalchemy import create_engine
+from utils import insert_teams_with_random_stats, get_team_ids
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
 from models import Base
+from models import Match
+from models import Team
 
 # Creamos un motor de base de datos usando la URL y habilitamos la opción
 # de impresión de comandos SQL (echo)
 engine = create_engine(database_url)
+
+# Crea un objeto Inspector para inspeccionar la base de datos
+inspector = inspect(engine)
 
 # Creamos una clase Session que se utilizará para interactuar con la base
 # de datos
@@ -16,6 +21,9 @@ session = Session()
 # Crear las tablas en la base de datos
 Base.metadata.create_all(engine)
 
+# Obtiene una lista de todas las tablas en la base de datos
+tabla_names = inspector.get_table_names()
+
 # Crear los equipos
 harry_potter_teams = [
     "Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw",
@@ -23,11 +31,21 @@ harry_potter_teams = [
     "Wizarding Warriors", "Spellcast Strikers", "Enchanted Defenders"
 ]
 
+season = 2
 
-# Ingresar equipos a la base de datos
-insert_teams_with_random_stats(harry_potter_teams, session=session)
 
-# Crear el calendario Fixture
-list_IDteams = get_team_ids(session)
-create_season(teams=list_IDteams, session=session)
-session.close()
+# Verifica si las tablas de Match y Team están en la lista de tablas
+if 'teams' in tabla_names and session.query(Team).count() == 0:
+    insert_teams_with_random_stats(harry_potter_teams, session=session)
+    print("Los equipos se han sido creados.")
+else:
+    print("Los equipos ya han sido creados.")
+
+if 'matches' not in tabla_names and session.query(Match).count() == 0:
+    # Crear el calendario Fixture
+    list_IDteams = get_team_ids(session)
+    # create_season(teams=list_IDteams, session=session, season=season)
+    session.close()
+    print("Los partidos se han sido creados.")
+else:
+    print("Los partidos ya han sido creados.")
